5f1247c8b9b3aa7d579985dd96f21c925397a128
Liam Barstad <liambarstad@gmail.com>
2017-09-12 16:42:09 -0600

    exclamation mark

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,7 +68,7 @@ RSpec.configure do |config|
 
   config.infer_spec_type_from_file_location!
 
-  config.filter_rails_from_backtrace
+  config.filter_rails_from_backtrace!
 end
 
 Shoulda::Matchers.configure do |config|
