ab628aad94bb15b2b1d8353a4b6ce09d233914d9
Daniel Phillips <danieledward.phillips@gmail.com>
2017-09-12 16:44:35 -0600

    add model for item

--- spec/features/items/see_index_spec.rb
+++ spec/features/items/see_index_spec.rb
@@ -4,9 +4,9 @@ require 'rails_helper'
 feature "As an unathenticated user" do
   scenario "sees all items" do
 
-  3.times do
-    item(:create)
-  end
+  Item.create(name: "Tablesalt", descrption: "its good", price: 1, stock_level: 1000, image: "google.com")
+  Item.create(name: "Tablesalt", descrption: "its good", price: 1, stock_level: 1000, image: "google.com")
+  Item.create(name: "Tablesalt", descrption: "its good", price: 1, stock_level: 1000, image: "google.com")
 
   visit items_path
 
