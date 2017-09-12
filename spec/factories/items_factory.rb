236fd8ee42110d6d80f78cf8e7eb64856da2ad1e
Liam Barstad <liambarstad@gmail.com>
2017-09-12 16:32:58 -0600

    add items factory, user can see items spec

---
+++ spec/factories/items_factory.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :item do
+    price 5.00
+    stock_level rand(0..10)
+    image "http://i0.kym-cdn.com/photos/images/facebook/000/918/952/d5a.png"
+    sequence :name do |i|
+      name "Sel"
+    end
+    description "insert lorem here"
+  end
+end
