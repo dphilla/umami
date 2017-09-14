ea604579ca71d427b32d3aba14f950698b3b462b
Liam Barstad <liambarstad@gmail.com>
2017-09-14 17:45:56 -0600

    add order and order_items

---
+++ spec/factories/order_items.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :order_item do
+    item nil
+    order nil
+  end
+end
---
+++ spec/features/orders/user_can_view_single_order_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "user can view single order" do
+  scenario "from orders page" do
+    user = create(:user)
+    visit root_path
+    click_on("Login")
+    fill_in "user[name]", with: user.name
+    fill_in "user[password]", with: "Password"
+    click_on("Log in")
+    order = create(:order, user: user)
+    item1 = order.items.create(:item)
+
+    visit orders_path
+    click_on order.created_at
+
+    expect(current_path).to eq(order_path(order))
+    expect(page).to have_content(item1.name)
+    expect(page).to have_content(item1.description)
+    expect(page).to have_content(item1.to_money)
+    expect(page).to have_content(order.status)
+    expect(page).to have_content(item1.description)
+    expect(page).to have_content(order.created_at)
+    expect(page).to have_content(order.total_price)
+  end
+end
