5d37a4b71e7787ddc1627596d4f8d03d1820cf20
Liam Barstad <liambarstad@gmail.com>
2017-09-14 16:53:16 -0600

    display all orders by created_at on /orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914033717) do
+ActiveRecord::Schema.define(version: 20170914223806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 20170914033717) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "status", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -52,4 +60,5 @@ ActiveRecord::Schema.define(version: 20170914033717) do
 
   add_foreign_key "item_tags", "items"
   add_foreign_key "item_tags", "tags"
+  add_foreign_key "orders", "users"
 end
--- spec/features/orders/user_can_see_all_orders_spec.rb
+++ spec/features/orders/user_can_see_all_orders_spec.rb
@@ -2,16 +2,21 @@ require 'rails_helper'
 
 RSpec.feature "user can see all orders" do
   scenario "from orders path" do
-
     user = create(:user)
+    visit root_path
+    click_on("Login")
+    fill_in "user[name]", with: user.name
+    fill_in "user[password]", with: "Password"
+    click_on("Log in")
+
     user2 = create(:user)
     order1, order2 = create_list(:order, 2, user: user)
     order3 = create(:order, user: user2)
 
     visit("/orders")
 
-    expect(page).to have_content(order1)
-    expect(page).to have_content(order2)
+    expect(page).to have_content(order1.created_at)
+    expect(page).to have_content(order2.created_at)
     expect(page).to_not have_content(order3)
   end
 end
