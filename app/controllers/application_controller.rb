c70ddf31a457c5d64cc478f2ebabb2e6945dfee6
Liam Barstad <liambarstad@gmail.com>
2017-09-16 15:07:43 -0600

    add current_user checks if ID exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] && User.exists?(id: session[:user_id])
   end
 
   def set_cart
