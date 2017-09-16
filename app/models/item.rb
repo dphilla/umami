a5a6118b65ca1ddbb4418f1ff3bfdb2ec5756149
Mike Heft <mikeheft@gmail.com>
2017-09-16 08:49:27 -0600

    Test validations on Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,10 @@
 class Item < ApplicationRecord
   has_many :item_tags, dependent: :destroy
   has_many :tags, through: :item_tags
-
+  validates :name, uniqueness: true, presence: true
+  validates :description, presence: true, uniqueness: true
+  validates :price, presence: true
+  
   enum status: [:Active, :Retired]
 
   def to_money
