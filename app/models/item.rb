37057f7666104d5d99525a0c9c6caa4a54c0a6e7
Daniel Phillips <danieledward.phillips@gmail.com>
2017-09-16 16:50:04 -0600

    fixes unique issue

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,8 @@ class Item < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
 
-  #validates :name, uniqueness: true, presence: true
-  # validates :description, presence: true, uniqueness: true
+  validates :name, presence: true
+   validates :description, presence: true
   validates :price, presence: true
 
 
