ded7d3be8a18991525376f0394c6056e42e757ec
Daniel Phillips <danieledward.phillips@gmail.com>
2017-09-16 16:13:36 -0600

    fixes seed file

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,10 @@ class Item < ApplicationRecord
   has_many :order_items
   has_many :orders, through: :order_items
 
-  validates :name, uniqueness: true, presence: true
+  #validates :name, uniqueness: true, presence: true
   # validates :description, presence: true, uniqueness: true
   validates :price, presence: true
-  
+
 
   enum status: [:Active, :Retired]
 
