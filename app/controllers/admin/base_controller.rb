62e8f82352f88a02a406e991a7773dbb0290f6b4
Mike Heft <mikeheft@gmail.com>
2017-09-16 13:18:06 -0600

    Add admin routes controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,4 +4,7 @@ class Admin::BaseController < ApplicationController
   def require_admin
     render file: '/public/404' unless current_admin?
   end
+  
+  def dashboard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
    resources :orders #we may take some routes out once it's more clear what we need.
  end
 
- get 'admin/dashboard' => 'admin#dashboard'
+ get 'admin/dashboard' => 'admin/base#dashboard'
  get "/dashboard" => "users#dashboard"
  get "/login" => "sessions#new"
  post "/login" => "sessions#create"
