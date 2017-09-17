0141466c4193fa17c9a4aadf51035b83e9b15e6f
Mike Heft <mikeheft@gmail.com>
2017-09-17 10:01:03 -0600

    Refactor to remove logic from controller and minor alignment in routes

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,19 +10,19 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def create
-    if params[:item][:image].empty?
-      params[:item][:image] = "http://img.providr.com/n-SALT-628x314.jpg"
-    end
-    if Item.find_by(name: params[:item][:name])
-      flash[:notice] = "Item with that name already exists"
-      redirect_to new_admin_item_path
-    elsif Item.find_by(description: params[:item][:description])
-      flash[:notice] = "Item with that description already exists"
-      redirect_to new_admin_item_path
-    elsif params[:item][:tag_ids].count == 1
-      flash[:notice] = "Item must have at least one tag"
-      redirect_to new_admin_item_path
-    else
+    # if params[:item][:image].empty?
+    #   params[:item][:image] = "http://img.providr.com/n-SALT-628x314.jpg"
+    # end
+    # if Item.find_by(name: params[:item][:name])
+    #   flash[:notice] = "Item with that name already exists"
+    #   redirect_to new_admin_item_path
+    # elsif Item.find_by(description: params[:item][:description])
+    #   flash[:notice] = "Item with that description already exists"
+    #   redirect_to new_admin_item_path
+    # elsif params[:item][:tag_ids].count == 1
+    #   flash[:notice] = "Item must have at least one tag"
+    #   redirect_to new_admin_item_path
+    # else
       @item = Item.new(item_params)
       if @item.save
         flash[:notice] = "#{@item.name} Created"
@@ -31,7 +31,7 @@ class Admin::ItemsController < Admin::BaseController
         flash[:notice] = "No fields can be empty"
         redirect_to new_admin_item_path
       end
-    end
+    # end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  root "items#index"
- resources :items, only: [:index, :show]
- resources :tags, only: [:index, :show]
- resources :users, only: [:new, :create]
+ resources :items,  only: [:index, :show]
+ resources :tags,   only: [:index, :show]
+ resources :users,  only: [:new, :create]
  resources :orders, only: [:index, :show, :create]
 
  namespace :admin do
@@ -13,13 +13,13 @@ Rails.application.routes.draw do
  end
 
  get 'admin/dashboard' => 'admin/base#dashboard'
- get "/dashboard" => "users#dashboard"
- get "/login" => "sessions#new"
- post "/login" => "sessions#create"
- delete "/logout" => "sessions#destroy"
- get "/cart" => "carts#show"
- post "/cart" => "carts#create"
- patch "/cart" => "carts#update"
- delete "/cart" => "carts#destroy"
- get "/:name" => "tags#show"
+ get "/dashboard"      => "users#dashboard"
+ get "/login"          => "sessions#new"
+ post "/login"         => "sessions#create"
+ delete "/logout"      => "sessions#destroy"
+ get "/cart"           => "carts#show"
+ post "/cart"          => "carts#create"
+ patch "/cart"         => "carts#update"
+ delete "/cart"        => "carts#destroy"
+ get "/:name"          => "tags#show"
 end
