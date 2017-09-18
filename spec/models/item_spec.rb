f72db88cb6bfc38f5d6833fcc787788ecc8f6c3e
Mike Heft <mikeheft@gmail.com>
2017-09-18 14:36:54 -0600

    Add validation for positive price

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   it {should validate_presence_of :name}
   it {should validate_presence_of :price}
+  it {should validate_numericality_of :price}
   it {should validate_presence_of :tags}
   it {should validate_presence_of :description}
 
