58450489ca94e41b9bd760520d32b2cbd0bce1bf
Mike Heft <mikeheft@gmail.com>
2017-09-18 14:24:14 -0600

    Add test for presence validations on Item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   it {should validate_presence_of :name}
   it {should validate_presence_of :price}
+  it {should validate_presence_of :tags}
+  it {should validate_presence_of :description}
 
   describe 'Instance Methods' do
     it '#to_money' do
