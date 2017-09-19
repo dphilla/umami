3e4172ad80899715b81baaf6a7bc88b013f8ca91
Aram Anderson <aram.j.anderson@gmail.com>
2017-09-19 14:46:52 -0600

    Add sendgrid api

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
-# Load the Rails application.
 require_relative 'application'
-
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'aram.j.anderson',
+  :password => 'C10g10ves0l0!',
+  :domain => 'https://umami-salt.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
 # Initialize the Rails application.
 Rails.application.initialize!
