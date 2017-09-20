474d7e43e5ab5dd9e4444a8a60b530d089de2538
Daniel Phillips <danieledward.phillips@gmail.com>
2017-09-19 19:38:27 -0600

    add methods on models for retaining state of prices for orders on checkout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916214237) do
+ActiveRecord::Schema.define(version: 20170919213230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170916214237) do
     t.bigint "order_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "item_price_record"
     t.index ["item_id"], name: "index_order_items_on_item_id"
     t.index ["order_id"], name: "index_order_items_on_order_id"
   end
