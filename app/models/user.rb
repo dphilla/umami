5642ef9adc348ef56e2ca2adc05a28ba693c6345
Mike Heft <mikeheft@gmail.com>
2017-09-20 12:05:05 -0600

    Add default image to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,29 @@ class User < ApplicationRecord
   validates_presence_of :name, :email, :address, :password
   validates_uniqueness_of :name, :email
   has_many :orders
+  
+  before_save :set_image
 
   enum role: [:default, :admin]
+  
+  def self.find_or_create_from_auth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.image = auth.info.image
+      user.address = "123 ABC St"
+      user.password = 'n/a'
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save
+    end
+  end
+
+  def set_image
+    if self[:image].empty? 
+      self[:image] = "http://www.diamondrayshk.com/images/default-user.png"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916214237) do
+ActiveRecord::Schema.define(version: 20170919222044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,11 @@ ActiveRecord::Schema.define(version: 20170916214237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "provider"
+    t.string "uid"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.string "image"
   end
 
   add_foreign_key "item_tags", "items"
